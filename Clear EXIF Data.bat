c9e9f075c6bf90b25236e653be66ca25341c8810
Chris <chrisdnorris@gmail.com>
2024-12-15 17:40:25 +0000

    Update Clear EXIF Data.bat

--- Clear EXIF Data.bat
+++ Clear EXIF Data.bat
@@ -1,5 +1,5 @@
 @echo off
 cls
-"C:\Users\Chris\Documents\Photo Tools\exiftool.exe" -all= --icc_profile:all -tagsfromfile @ -exif:ImageDescription -exif:DateTimeOriginal -exif:CreateDate -xmp:description="Photograph" -artist="Chris Norris chrisdnorris@gmail.com" -copyright="Chris Norris chrisdnorris@gmail.com" %1
+for %%f in (*.jpg) do "C:\Users\Chris\Documents\Photo Tools\exiftool.exe" -all= --icc_profile:all -tagsfromfile @ -exif:ImageDescription -exif:DateTimeOriginal -exif:CreateDate -xmp:description="Photograph" -artist="Chris Norris chrisdnorris@gmail.com" -copyright="Chris Norris chrisdnorris@gmail.com" %%~nf.jpg
 
 pause
