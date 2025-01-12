1f34d67ef1d3a6c9f6e0cb2779c92ae75366ecad
Chris <chrisdnorris@gmail.com>
2025-01-12 20:14:30 +0000

    Update Auto Import Photos.bat

--- Auto Import Photos.bat
+++ Auto Import Photos.bat
@@ -17,8 +17,10 @@ set finalMonth=!monthText:~%theMonth%,3!
 mkdir "C:\Users\Chris\Pictures\Develop\%date:~0,4%.%month% %finalMonth% %date:~8,2%"
 
 if exist "F:\DCIM\101D3400" goto import1
+set count1=0
 :start
 if exist "G:\DCIM\101ND200" goto import2
+set count2=0
 goto last
 
 :import1
